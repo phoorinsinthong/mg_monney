1a304d676023ba2556da5a0166481a6d2323b11c
PhoorinS <91134913+PhoorinS@users.noreply.github.com>
2026-05-28 14:28:15 +0700

    chore: change Fuse.js search threshold to 0.4 for higher precision

--- src/services/pensionService.js
+++ src/services/pensionService.js
@@ -33,7 +33,7 @@ class PensionService {
         { name: 'topic', weight: 2 },
         { name: 'answer', weight: 1 },
       ],
-      threshold: 0.6,
+      threshold: 0.4,
       includeScore: true,
     });
   }
